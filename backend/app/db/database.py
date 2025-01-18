5ade61adc47f029a1f06eb79f1cc46a81076c010
Amado Rosas <amadorosas97@gmail.com>
2025-01-18 00:28:01 -0700

    Folder structure modifications

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,13 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Database URL (replace with your own database URL)
-SQLALCHEMY_DATABASE_URL = "sqlite:///../project.db"
 
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-# DATABASE_PATH = os.path.join(BASE_DIR, "project.db")
-
-# SQLALCHEMY_DATABASE_URL = f"sqlite:///{DATABASE_PATH}"
+SQLALCHEMY_DATABASE_URL = "sqlite:///../medical_intership.db"
 
 try:
     engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
