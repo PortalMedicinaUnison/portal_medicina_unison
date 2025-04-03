25f982177b04a1a5de5085a1807693310af019d4
Amado Rosas <amadorosas97@gmail.com>
2025-04-03 11:33:52 -0700

    style: Enhance Navbar and Sidebar components with improved layout, icon updates, and additional menu items for better user experience

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -16,8 +16,8 @@ function Sidebar({ toggleSidebar }) {
                         </a>
                     </div>
                 </div>
-                <div className="sidebar">
-                    <ul className="space-y-1 font-medium">
+                <div className="sidebar ">
+                    <ul className="space-y-1">
                         <li>
                             <a href="#" className="sidebar-item-group group">
                                 <svg
@@ -109,18 +109,63 @@ function Sidebar({ toggleSidebar }) {
                                     className="sidebar-item-icon"
                                     viewBox="-1 -1 23 23"
                                     aria-hidden="true"
+                                    fill="none"
                                     stroke="currentColor"
                                     stroke-linecap="round"
                                     stroke-linejoin="round"
                                 >
-                                    <circle cx="10.5" cy="10.5" r="8" fill="none"/>
+                                    <circle cx="10.5" cy="10.5" r="8"/>
                                     <path d="M10.5 11.5v-5"/>
-                                    <circle cx="10.5" cy="14.5" r="1" fill="currentColor"/>
+                                    <circle cx="10.5" cy="14.5" r="1"/>
                                 </svg>                        
                                 <span className="sidebar-item-text">Reportes</span>
                             </a>
                         </li>                        
                     </ul>
+                    
+                    <div>
+                        <ul>
+                            <li >
+                                <a href="#" className="sidebar-item-group group ">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="sidebar-item-icon"
+                                        viewBox="-1 -1 23 23"
+                                        aria-hidden="true"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <circle cx="8.5" cy="8.5" r="8"/>
+                                        <path d="M8.5 9.5v-1l1.414-1.414a2 2 0 0 0 .586-1.414V5.5c0-.613-.346-1.173-.894-1.447l-.212-.106a2 2 0 0 0-1.788 0L7.5 4c-.613.306-1 .933-1 1.618V6.5"/>
+                                        <circle cx="8.5" cy="12.5" r="1"/>
+                                    </svg>                        
+                                    <span className="sidebar-item-text">Preguntas frecuentes</span>
+                                </a>
+                            </li>
+                            <li >
+                                <a href="#" className="sidebar-item-group group ">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        className="sidebar-item-icon"
+                                        viewBox="-1 -1 23 23"
+                                        aria-hidden="true"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                    >
+                                        <circle cx="8.5" cy="8.5" r="8" fill="none"/>
+                                        <path d="M8.5 12.5v-4h-1m0 4h2"/>
+                                        <circle cx="8.5" cy="5.5" r="1"/>
+                                    </svg>                        
+                                    <span className="sidebar-item-text">Contacto</span>
+                                </a>
+                            </li>
+
+                        </ul>
+                    </div>         
                 </div>
             </aside>
         </div>
