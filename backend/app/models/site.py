82a90134b14007fd2bcfe9f3fb933035ec52eec7
alesanchezb <sanchezbraulio@gmail.com>
2025-05-12 19:16:45 -0700

    refactor(site): modify model site and was added a new model with name Institution, where there is a connection beetwen Institution and Site

--- backend/app/models/site.py
+++ backend/app/models/site.py
@@ -6,6 +6,16 @@ from .base import BaseModel
 from .types import IntEnumType
 
 
+class Institution(BaseModel):
+    __tablename__ = 'institutions'
+    
+    institution_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    
+    def __repr__(self):
+        return f"<Institution(name={self.name})>"
+
+
 class SiteTypeEnum(IntEnum):
     HOSPITAL = 1
     CLINIC = 2
@@ -17,13 +27,15 @@ class Site(BaseModel):
     
     site_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    institution_id: Mapped[int] = mapped_column(Integer, ForeignKey("institutions.institution_id"), nullable=False)
     site_type: Mapped[SiteTypeEnum] = mapped_column(IntEnumType(SiteTypeEnum), nullable=False)
     address: Mapped[str] = mapped_column(String(255))
     city: Mapped[str] = mapped_column(String(50))
     state: Mapped[str] = mapped_column(String(50))
     country: Mapped[str] = mapped_column(String(50))
     capacity: Mapped[int] = mapped_column(Integer)
-    contact_name: Mapped[str] = mapped_column(String(100), nullable=False)
+    head_of_education: Mapped[str] = mapped_column(String(100), nullable=False)
+    deputy_head_of_education: Mapped[str] = mapped_column(String(100), nullable=False)
     contact_email: Mapped[str] = mapped_column(String(50))
     contact_phone: Mapped[str] = mapped_column(String(15))
     is_available: Mapped[bool] = mapped_column(Boolean, nullable=False, default=True)
