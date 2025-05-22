00ccb02bc78418ef322879f435114007bf700b9c
alesanchezb <sanchezbraulio@gmail.com>
2025-05-21 20:35:50 -0700

    It was added scheme, model and repo to site and institution.

--- backend/app/schemas/site.py
+++ backend/app/schemas/site.py
@@ -5,20 +5,27 @@ from utils.validation import is_valid_email
 
 
 class SiteInput(BaseModel):
+    institution_id: int
     admin_id: int
     name: str
     site_type: SiteTypeEnum
-    address: Optional[str] = None
-    city: Optional[str] = None
-    state: Optional[str] = None
-    country: Optional[str] = None
-    capacity: Optional[int] = None
-    contact_name: str
+    address: str
+    city: str
+    state: str
+    capacity: int
+    director: str
+    subdirector: str
     contact_email: str
     contact_phone: Optional[str] = None
+    
     is_available: bool = True
 
     @field_validator("contact_email")
     def validate_email(cls, email):
         is_valid_email(email)
         return email
+
+
+class InstitutionInput(BaseModel):
+    name: str
+    
