55014787a9a377816b319f5af26e17fd05eb12c1
Amado Rosas <amadorosas97@gmail.com>
2025-07-15 13:40:21 -0700

    feat(site): Refactor SiteInput schema and add tests for institution and site functionalities

--- backend/app/schemas/site.py
+++ backend/app/schemas/site.py
@@ -1,30 +1,35 @@
 from pydantic import BaseModel, field_validator
-from models.site import SiteTypeEnum
 from typing import Optional
 from utils.validation import is_valid_email
 
 #---------------SITE-------------------
 class SiteInput(BaseModel):
     institution_id: int
-    admin_id: int
     name: str
-    site_type: SiteTypeEnum
     address: str
     city: str
-    state: str
     capacity: int
-    director: str
-    subdirector: str
-    contact_email: str
-    contact_phone: Optional[str] = None
+    teaching_head_name: str
+    teaching_head_email: Optional[str] = None
+    teaching_head_phone: Optional[str] = None
+    teaching_deputy_name: Optional[str] = None
+    teaching_deputy_email: Optional[str] = None
+    teaching_deputy_phone: Optional[str] = None
     
     is_available: bool = True
 
-    @field_validator("contact_email")
+    @field_validator("teaching_head_email")
     def validate_email(cls, email):
-        is_valid_email(email)
+        if email:
+            is_valid_email(email)
         return email
     
+    @field_validator("teaching_deputy_email")
+    def validate_deputy_email(cls, email):
+        if email:
+            is_valid_email(email)
+        return email
+
 #---------------INSTITUTION-------------------
 class InstitutionInput(BaseModel):
     name: str
