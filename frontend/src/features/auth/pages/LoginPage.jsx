5e0d8aff247747f1bb7fd5a776a185c579d25090
Amado Rosas <amadorosas97@gmail.com>
2025-07-18 13:23:20 -0700

    feat(auth): Enhance Login and Register pages with new layout and styling

--- frontend/src/features/auth/pages/LoginPage.jsx
+++ frontend/src/features/auth/pages/LoginPage.jsx
@@ -3,13 +3,47 @@ import LoginForm from '../components/LoginForm';
 
 function LoginPage() {
   return (
-    <div className="flex min-h-screen flex-1 flex-col justify-center lg:px-8">
-      <div className='px-10 py-10 mx-auto sm:mx-auto sm:w-full sm:max-w-sm rounded-[3vw] border'>
-          <h2 className="login-title">Portal de Servicio Social</h2>
+
+  <div className="flex flex-col min-h-screen bg-gray-100">
+      <header className="bg-white w-full">
+        <div className="flex px-4 py-4">
+          <a
+            href="#"
+            className="border-b-4 border-gray-700 pb-2 text-2xl font-bold text-black"
+          >
+            Portal internado-servicio social
+          </a>
+        </div>
+      </header>
+
+    <div className="flex flex-1">      
+      <div className="hidden md:flex w-1/2 items-center justify-center">
+        <img 
+          className="w-3/4 h-3/4 object-contain" 
+          src="../../../../assets/started3.svg" 
+        />
+      </div>
+
+    
+
+    {/* Formulario */}
+      <div className="flex min-h-screen flex-1 flex-col justify-center lg:px-8">
+        <div className='px-10 py-10 mx-auto sm:mx-auto sm:w-full sm:max-w-sm rounded-[3vw] border bg-white'>
+            <h2 className="login-title">
+              Inicio sesión
+            </h2>
             <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
               <LoginForm />
-          </div>
+            </div>
+        </div>
       </div>
+    </div>
+
+        <footer className="bg-[#24398A] w-full py-4 mt-auto">
+        <div className="text-center text-xs text-white">
+          © 2025 Unison. Todos los derechos reservados.
+        </div>
+      </footer>
     </div>
   );
 }
--- frontend/src/features/auth/pages/RegisterPage.jsx
+++ frontend/src/features/auth/pages/RegisterPage.jsx
@@ -3,14 +3,40 @@ import RegisterForm from '../components/RegisterForm';
 
 function RegisterPage() {
   return (
+        
+    <div className="flex flex-wrap">
+      {/* Columna izquierda */}
+      <div className="flex w-full flex-col md:w-1/2">
+        <div className="flex justify-center pt-12 md:-mb-24 md:justify-start md:pl-12">
+          <a
+            href="#"
+            className="border-b-4 border-gray-700 pb-2 text-2xl font-bold text-gray-900"
+          >
+            Unison.
+          </a>
+        </div>
+    
     <div className="flex min-h-screen flex-1 flex-col justify-center lg:px-8">
       <div className="px-10 py-10 mx-auto sm:w-full sm:max-w-md rounded-[3vw] border">
-        <h2 className="login-title">Portal de Servicio Social</h2>
+        <h2 className="login-title">Registrarse</h2>
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
           <RegisterForm />
         </div> 
       </div>
     </div>
+      </div> {/* ← Aquí cerramos la columna izquierda */}
+
+
+    <div className="pointer-events-none relative hidden h-screen select-none bg-black md:block md:w-1/2">
+      <div className="absolute bottom-0 z-10 px-8 text-white opacity-100">
+        <p className="mb-8 text-3xl font-semibold leading-10">We work 10x faster than our compeititors and stay consistant. While they're bogged won with techincal debt, we're realeasing new features.</p>
+        <p className="mb-4 text-3xl font-semibold">John Elmond</p>
+        <p className="">Founder, Emogue</p>
+        <p className="mb-7 text-sm opacity-70">Web Design Agency</p>
+      </div>
+      <img className="-z-1 absolute top-0 h-full w-full object-cover opacity-90" src="https://images.unsplash.com/photo-1565301660306-29e08751cc53?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80" />
+    </div>
+</div>
   );
 }
 
