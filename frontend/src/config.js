a628f722b8c1e55046cbff72854613b6b9a72cd3
Amado Rosas <amadorosas97@gmail.com>
2025-09-11 11:24:34 -0700

    feat(admin): Update admin routes and config for promotions, sites, institutions, announcements, and surveys

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -17,34 +17,49 @@ export const ROUTES = {
     },
 
     ADMIN: {
-        SITE_LIST: 'sites',
-        SITE_CREATE: 'sites/create',
-        SITE_INFO: (siteId) => `sites/${siteId}`,
-
         USER_LIST: 'users',
         USER_DETAIL: (userId) => `users/${userId}`,
 
+        SITE_LIST: 'sites',
+        SITE_CREATE: 'sites/create',
+        SITE_DETAIL: (siteId) => `sites/${siteId}`,
+        SITE_EDIT: (siteId) => `sites/${siteId}/edit`,
+        SITE_DELETE: (siteId) => `sites/${siteId}/delete`,
+
+
         PROMOTION_LIST: 'promotions',
-        PROMOTION_DETAIL: (promotionId) => `promotions/${promotionId}`,
         PROMOTION_CREATE: 'promotions/create',
+        PROMOTION_DETAIL: (promotionId) => `promotions/${promotionId}`,
+        PROMOTION_EDIT: (promotionId) => `promotions/${promotionId}/edit`,
+        PROMOTION_DELETE: (promotionId) => `promotions/${promotionId}/delete`,
+
+        PSD_LIST: 'psd',
+        PSD_CREATE: 'psd/create',
+        PSD_DETAIL: (psdId) => `psd/${psdId}`,
+        PSD_EDIT: (psdId) => `psd/${psdId}/edit`,
+        PSD_DELETE: (psdId) => `psd/${psdId}/delete`,
 
         REPORT_LIST: 'reports',
         REPORT_DETAIL: (reportId) => `reports/${reportId}`,
+        REPORT_CREATE: 'reports/create',
+        REPORT_EDIT: (reportId) => `reports/${reportId}/edit`,
+        REPORT_DELETE: (reportId) => `reports/${reportId}/delete`,
 
         ANNOUNCEMENTS_LIST: 'announcements',
-        ANNOUNCEMENT_DETAIL: (announcementId) => `announcements/${announcementId}`,
         ANNOUNCEMENT_CREATE: 'announcements/create',
+        ANNOUNCEMENT_DETAIL: (announcementId) => `announcements/${announcementId}`,
+        ANNOUNCEMENT_EDIT: (announcementId) => `announcements/${announcementId}/edit`,
         ANNOUNCEMENT_DELETE: (announcementId) => `announcements/${announcementId}/delete`,
 
         INSTITUTION_LIST: 'institutions',
-        INSTITUTION_DETAIL: (institutionId) => `institutions/${institutionId}`,
         INSTITUTION_CREATE: 'institutions/create',
+        INSTITUTION_DETAIL: (institutionId) => `institutions/${institutionId}`,
         INSTITUTION_EDIT: (institutionId) => `institutions/${institutionId}/edit`,
         INSTITUTION_DELETE: (institutionId) => `institutions/${institutionId}/delete`,
 
         SURVEY_LIST: 'surveys',
-        SURVEY_DETAIL: (surveyId) => `surveys/${surveyId}`,
         SURVEY_CREATE: 'surveys/create',
+        SURVEY_DETAIL: (surveyId) => `surveys/${surveyId}`,
         SURVEY_EDIT: (surveyId) => `surveys/${surveyId}/edit`,
         SURVEY_DELETE: (surveyId) => `surveys/${surveyId}/delete`,
     }
