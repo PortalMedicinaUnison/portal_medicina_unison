f905bbffc3e25d44eb196f8058902d428cfaceb9
Amado Rosas <amadorosas97@gmail.com>
2025-09-20 16:32:16 -0700

    style(announcements): Clean up formatting and improve code readability in announcement pages

--- frontend/src/admin/features/announcements/pages/AnnouncementFormPage.jsx
+++ frontend/src/admin/features/announcements/pages/AnnouncementFormPage.jsx
@@ -2,16 +2,17 @@ import Layout from '../../../../Layout';
 import PageLayout from '../../../../components/PageLayout';
 import AnnouncementsForm from '../components/AnnouncementsForm';
 
+
 function AnnouncementFormPage() {
-    return (
-        <Layout>
-        <PageLayout 
-            title="📣 Registrar nuevo anuncio" 
-        >
-            <AnnouncementsForm/>
-        </PageLayout>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <PageLayout 
+        title="📣 Registrar nuevo anuncio" 
+      >
+        <AnnouncementsForm/>
+      </PageLayout>
+    </Layout>
+  );
 }
 
 export default AnnouncementFormPage;
--- frontend/src/admin/features/announcements/pages/AnnouncementsListPage.jsx
+++ frontend/src/admin/features/announcements/pages/AnnouncementsListPage.jsx
@@ -7,36 +7,36 @@ import AnnouncementsList from '../components/AnnouncementsList';
 
 
 function AnnouncementsListPage() {
-    const navigate = useNavigate();
-    const { announcements, loading: fetching, error: fetchError, refetch } = useGetAnnouncements();
+  const navigate = useNavigate();
+  const { announcements, loading: fetching, error: fetchError, refetch } = useGetAnnouncements();
     
-    const announcementActions = (
-        <span className="show-on-sm">
-        <button
-            type="button"
-            className="btn-primary"
-            onClick={() => navigate(adminAbs(ROUTES.ADMIN.ANNOUNCEMENT_CREATE))}
-        >
-            Crear
-        </button>
-        </span>
-    );
+  const actions = (
+    <span className="show-on-sm">
+      <button
+        type="button"
+        className="btn-primary"
+        onClick={() => navigate(adminAbs(ROUTES.ADMIN.ANNOUNCEMENT_CREATE))}
+      >
+        Crear
+      </button>
+    </span>
+  );
     
-    return (
-        <Layout>
-            <PageLayout 
-            title="Lista de anuncios"
-            actions={announcementActions}
-            >
-            <AnnouncementsList
-                announcements={announcements}
-                fetching={fetching}
-                fetchError={fetchError}
-                refetch={refetch}
-             />
-            </PageLayout>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <PageLayout 
+        title="Lista de anuncios"
+        actions={actions}
+      >
+        <AnnouncementsList
+          announcements={announcements}
+          fetching={fetching}
+          fetchError={fetchError}
+          refetch={refetch}
+        />
+      </PageLayout>
+    </Layout>
+  );
 }
 
 export default AnnouncementsListPage;
